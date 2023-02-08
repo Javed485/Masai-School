0d880f42499202ac08bd8c0f8f7267d94298a02b
Mohammad Javed <javedmohammad485@gmail.com>
2023-02-09 01:57:35 +0530

    Upload

--- Js advance/Search Data/search.js
+++ Js advance/Search Data/search.js
@@ -9,14 +9,38 @@
 // }
 
 async function searchMovie(){
+    let loader_div = document.getElementById('loader_div');
+    loader_div.style.display = 'block';
+
     let movie = document.getElementById('movie_name').value;
     try{
         let response = await fetch(`http://www.omdbapi.com/?apikey=f3d5c71f&s=${movie}`);
         let data = await response.json();
         let actualData = data.Search;
-        console.log('actualData', actualData);
+        // console.log('actualData', actualData);
+        appendMovie(actualData);
     }
     catch(err){
         console.log('err', err);
     }
+}
+
+function appendMovie(data){
+    let loader_div = document.getElementById('loader_div');
+    loader_div.style.display = 'none';
+
+    let movie_div = document.getElementById('movie');
+    movie_div.innerHTML = null;
+    data.forEach(function(el){
+        let div = document.createElement('div');
+
+        let img = document.createElement('img');
+        img.src = el.Poster;
+
+        let p_name = document.createElement('p');
+        p_name.innerText = el.Title;
+
+        div.append(img, p_name);
+        movie_div.append(div);
+    });
 }
