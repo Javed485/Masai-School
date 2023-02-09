cdc4865365a40c1e14c9b995d76da2ca98dd61c0
Mohammad Javed <javedmohammad485@gmail.com>
2023-02-09 16:00:27 +0530

    Upload

--- Js advance/Movie app/script/index.js
+++ Js advance/Movie app/script/index.js
@@ -97,6 +97,22 @@ videoCard.forEach(function(item){
     })
 })
 
+// card sliders
+let cardContainers = [...document.querySelectorAll('.card-container')];
+let preBtns = [...document.querySelectorAll('.pre-btn')];
+let nxtBtns = [...document.querySelectorAll('.nxt-btn')];
 
+cardContainers.forEach(function(item, i){
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtns[i].addEventListener('click', function() {
+        item.scrollLeft = item.scrollLeft + containerWidth - 200;
+    })
+
+    preBtns[i].addEventListener('click', function() {
+        item.scrollLeft -= containerWidth + 200;
+    })
+})
 
 
