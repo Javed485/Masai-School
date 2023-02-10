10dff7eac5760ae3cde4531f62949fe97d058f72
Mohammad Javed <javedmohammad485@gmail.com>
2023-02-10 15:53:03 +0530

    Upload

--- Js advance/Movie app/script/latest.js
+++ Js advance/Movie app/script/latest.js
@@ -56,44 +56,37 @@ let latestShows = [
 ];
 
 let cardContainer = document.querySelector('.m-container');
-let cards = [];
-for(let cardIndex=0; cardIndex<latestShows.length; cardIndex++){
 
+let createCards = function() {
+    latestShows.forEach(function(el) {
+        let card = document.createElement('div');
+        card.className = 'card';
 
-let createCards = () => {
-    let card = document.createElement('div');
-    let imgElement = document.createElement('img');
-    let cardBody = document.createElement('div');
-    let h2 = document.createElement('h2');
-    let h6 = document.createElement('h6');
-    let btn = document.createElement('button');
-    // Attaching all Elements
-    imgElement.appendChild(document.createTextNode(''));  
-    h2.appendChild(document.createTextNode(latestShows[cardIndex].name));
-    h6.appendChild(document.createTextNode(latestShows[cardIndex].des));
-    btn.appendChild(document.createTextNode(latestShows[cardIndex].watchlistBtn));
-    cardBody.appendChild(h2);
-    cardBody.appendChild(h6);
-    cardBody.appendChild(btn);
-    card.appendChild(cardBody);
-    card.appendChild(imgElement);
-    cardContainer.appendChild(card);
-    
-    imgElement.src = latestShows[cardIndex].image;
-    // cardIndex++; 
+        let imgElement = document.createElement('img');
+        imgElement.src = el.image;
+        imgElement.className = 'card-img';
 
-    // Settinig Elements classname
-    card.className = 'card';
-    imgElement.className = 'card-img';
-    cardBody.className = 'card-body';
-    h2.className = 'name';
-    h6.className = 'des';
-    btn.className = 'watchlist-btn';
+        let cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
 
-    cards.push(card);
-    console.log(cards);   
+        let h2 = document.createElement('h2');
+        h2.innerText = el.title;
+        h2.className = 'name';
+
+        let h6 = document.createElement('h6');
+        h6.innerText = el.des;
+        h6.className = 'des';
+
+        let btn = document.createElement('button');
+        btn.innerText = el.watchlistBtn;
+        btn.className = 'watchlist-btn';
+
+        cardBody.append(h2, h6, btn);
+        card.append(imgElement, cardBody);
+        cardContainer.append(card);
+    });
 }
 createCards();
-}
+
 
 
